f32dfbf66937a4fef1a5a4272191e4ada6a0647d
Rakinul Alam <>
2026-09-05 03:15:44 +0600

    installed stripe and setup stripe for use

--- src/config/index.ts
+++ src/config/index.ts
@@ -6,7 +6,9 @@ const config = {
   PORT: env.PORT,
   DATABASE_URL:process.env.DATABASE_URL,
   JWT_REFRESH_SECRET:process.env.JWT_REFRESH_SECRET as string,
-  JWT_ACCESS_SECRET:process.env.JWT_ACCESS_SECRET as string
+  JWT_ACCESS_SECRET:process.env.JWT_ACCESS_SECRET as string,
+  STRIPE_SECRET_KEY:process.env.STRIPE_SECRET_KEY as string,
+  STRIPE_PUBLISHABLE_KEY:process.env.IPE_PUBLISHABLE_KEY as string,
 
 };
 
