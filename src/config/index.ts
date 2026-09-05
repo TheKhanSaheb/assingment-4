bfc2bb89ece0103a0b6e7abc701f739e9df967df
Rakinul Alam <>
2026-09-05 23:41:06 +0600

    changes stripe creditial at config

--- src/config/index.ts
+++ src/config/index.ts
@@ -8,7 +8,7 @@ const config = {
   JWT_REFRESH_SECRET:process.env.JWT_REFRESH_SECRET as string,
   JWT_ACCESS_SECRET:process.env.JWT_ACCESS_SECRET as string,
   STRIPE_SECRET_KEY:process.env.STRIPE_SECRET_KEY as string,
-  STRIPE_PUBLISHABLE_KEY:process.env.IPE_PUBLISHABLE_KEY as string,
+  
 
 };
 
