fb5a51677c1b0ad565a1a49ff06bfa3c8ab82a9e
Rakinul Alam <>
2026-09-05 23:40:16 +0600

    added admin service

--- src/modules/admin/admin.service.ts
+++ src/modules/admin/admin.service.ts
@@ -38,6 +38,15 @@ const updateUserStatus = async (
     data: {
       status,
     },
+    select: {
+      id: true,
+      name: true,
+      email: true,
+      role: true,
+      status: true,
+      createdAt: true,
+      updatedAt: true,
+    },
   });
 };
 
